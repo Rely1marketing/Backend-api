12d9774660ecf842383331f83b1a18ce55d7c910
Rely1marketing <prjnea@gmail.com>
2025-07-18 17:15:55 +0200

    Update server.js

--- server.js
+++ server.js
@@ -24,6 +24,10 @@ app.post("/api/messages", async (req, res) => {
   }
 });
 
-app.listen(process.env.PORT || 5000, () =>
-  console.log(`Servern körs på port ${process.env.PORT || 5000}`)
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () =>
+  console.log(`Servern körs på port ${PORT}`)
+);
+
 );
